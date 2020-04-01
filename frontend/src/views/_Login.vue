4cc016cdd7bc80bcfc108d631e526f2be4da2832
Byamungu Kabiraba <byamuk16@student.uia.no>
2020-04-02 00:25:37 +0200

    new frontend - register page remain

---
+++ frontend/src/views/_Login.vue
@@ -0,0 +1,75 @@
+<template>
+<!-- Outer Row -->
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-5 col-lg-12 col-md-9">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                            </div>
+                            <form class="user" @submit.prevent="handleSubmit">
+                                <div class="form-group">
+                                    <input type="text"  v-model="username" class="form-control form-control-user" :class="{ 'is-invalid': submitted && !password }" id="InputUsername" placeholder="Enter Username..." />
+                                    <div v-show="submitted && !username" class="invalid-feedback">Username is required</div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" v-model="password" class="form-control form-control-user" :class="{ 'is-invalid': submitted && !password }" id="InputPassword" placeholder="Password">
+                                    <div v-show="submitted && !password" class="invalid-feedback">Password is required</div>
+                                </div>
+                                <button class="btn btn-primary btn-user btn-block" :disabled="status.loggingIn">
+                                    Login
+                                </button>
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="#">Forgot Password?</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import { mapState, mapActions } from 'vuex'
+    export default {
+        data () {
+            return {
+                username: '',
+                password: '',
+                submitted: false
+            }
+        },
+        computed: {
+            ...mapState('account', ['status'])
+        },
+        created () {
+            // reset login status
+            this.logout();
+        },
+        methods: {
+            ...mapActions('account', ['login', 'logout']),
+            handleSubmit () {
+                this.submitted = true;
+                const { username, password } = this;
+                if (username && password) {
+                    this.login({ username, password })
+                }
+            }
+        }
+    };
+</script>
+
+<style>
+    .bg-gradient-dark {
+        background-color: #132235;
+        background-image: linear-gradient(180deg,#132235 10%,#224abe 100%);
+        background-size: cover;
+    }
+</style>
