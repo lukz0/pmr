617b0707b03226a3b99f01287b5b9207ee18d9be
Lukasz Filip Mrozik <lukafm18@student.uia.no>
2020-04-15 12:42:53 +0200

    rest test

--- frontend/cypress/integration/mirdash.test.js
+++ frontend/cypress/integration/mirdash.test.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 const baseurl = 'http://localhost:5000/';
+const user = {};
 
 context('Mirdash', () => {
     beforeEach(() => {
@@ -12,13 +13,40 @@ context('Mirdash', () => {
 
         cy.location('pathname').should('eq', '/login');
 
-        cy.get('input#Username').type('null');
-        cy.get('input#Password').type('null');
-        cy.get('button[type="submit"]').click();
+        cy.get('#Username').type('null');
+        cy.get('#Password').type('null');
+        cy.get('#Submit').click();
 
         cy.location('pathname').should('eq', '/login');
-        cy.get('.alert')
+        cy.get('#Alert')
             .should('be.visible')
             .should('contain.text', 'incorrect');
     });
-})
+
+    it('register', () => {
+        user.username = Math.random().toString(36).substring(2);
+        cy.visit(`${baseurl}register`)
+            .location('pathname').should('eq', '/register');
+        
+        cy.get('#Firstname').type('John');
+        cy.get('#Lastname').type('Smith');
+        cy.get('#Username').type(user.username);
+        cy.get('#Password').type('Password1.');
+        cy.get('#Submit').click();
+
+        cy.get('#Alert')
+            .should('be.visible')
+            .should('contain.text', 'Registration successful');
+    });
+
+    it('login', () => {
+        cy.visit(`${baseurl}login`)
+            .location('pathname').should('eq', '/login');
+
+        cy.get('#Username').type(user.username);
+        cy.get('#Password').type('Password1.');
+        cy.get('#Submit').click();
+
+        cy.location('pathname').should('eq', '/');
+    })
+});
