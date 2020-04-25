9f5f61ee79f69a6681ac1e622f077972fa2d5bb8
Byamungu Kabiraba <byamuk16@student.uia.no>
2020-04-25 15:54:03 +0200

    updated view user details

--- frontend/src/components/UserManager/Users.vue
+++ frontend/src/components/UserManager/Users.vue
@@ -12,33 +12,47 @@
             <template v-slot:cell(Edit)="data">
                 <b-button-group class="mx-1">
                     <b-button variant="info" @click="loadUser(data.item)">
-                        <b-icon-person-bounding-box />
-                        View/Edit</b-button>
-                    <b-button variant="warning" :id="`UsermanagerDelete${data.item.username}`" v-if="profile.id !== data.item.id"
+                        <b-icon-person-lines-fill/>
+                        View/Edit
+                    </b-button>
+                    <b-button variant="warning" :id="`UsermanagerDelete${data.item.username}`"
+                              v-if="profile.id !== data.item.id"
                               @click="deleteUser({id: data.item.id, api: $api})">
-                        <b-icon-person-dash />
+                        <b-icon-person-dash/>
                         Delete
                     </b-button>
                 </b-button-group>
             </template>
         </b-table>
 
-        <b-modal v-model="modalShow">
-            <div class="card">
-                <img src="https://via.placeholder.com/286x180?text=Bruk" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Cras justo odio</li>
-                    <li class="list-group-item">Dapibus ac facilisis in</li>
-                    <li class="list-group-item">Vestibulum at eros</li>
-                </ul>
-                <div class="card-body">
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
+        <b-modal v-model="modalShow" header-bg-variant="dark" header-text-variant="light">
+            <template v-slot:modal-title>
+                <b-icon-person-bounding-box/>
+                {{user.firstName}} {{user.lastName}} profile card
+            </template>
+            <div>
+                <table class="table table-borderless table-hover">
+                    <tr>
+                        <th>UserId</th>
+                        <td>{{user.id}}</td>
+                    </tr>
+                    <tr>
+                        <th>Name</th>
+                        <td>{{user.firstName}} {{user.lastName}} </td>
+                    </tr>
+                    <tr>
+                        <th>Username</th>
+                        <td>{{user.username}}</td>
+                    </tr>
+                    <tr>
+                        <th>Email</th>
+                        <td>{{user.email}}</td>
+                    </tr>
+                    <tr>
+                        <th>Role</th>
+                        <td>{{user.role}}</td>
+                    </tr>
+                </table>
             </div>
         </b-modal>
     </div>
@@ -51,23 +65,23 @@
         data() {
             return {
                 modalShow: false,
-                user: {'Id': '', firstName:'', lastName:'', username:''},
+                user: {'Id': '', firstName: '', lastName: '', username: ''},
                 fields: [
-                    { key: 'username', label: 'Username', sortable: true },
+                    {key: 'username', label: 'Username', sortable: true},
                     {
                         key: 'firstName',
                         label: 'Full name',
                         sortable: true,
                         formatter: (value, key, item) => item.firstName + ' ' + item.lastName
                     },
-                    { key: 'email', label: 'Email address', sortable: true },
+                    {key: 'email', label: 'Email address', sortable: true},
                     {
                         key: 'role',
                         label: 'Policy',
                         sortable: true,
                         formatter: (value, key, item) => item.role != null ? item.role : 'User'
                     },
-                    { key: 'Edit', label: '#' }]
+                    {key: 'Edit', label: '#'}]
             }
         },
         created() {
