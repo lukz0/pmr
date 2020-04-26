ed1631c47bbb548229e4c72a98bd226b77ec1a92
Byamungu Kabiraba <byamuk16@student.uia.no>
2020-04-26 12:40:57 +0200

    designed the alert box

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <b-alert v-if="alert.message" :class="`alert ${alert.type}`" id="Alert" show>{{alert.message}}</b-alert>
+<!--    <b-alert v-if="alert.message" :class="`alert ${alert.type}`" id="Alert" show>{{alert.message}}</b-alert>-->
+    <b-modal
+            v-model="alert.type"
+            :header-bg-variant="headerBgVariant(alert.type)"
+            ok-only>
+        <template v-slot:modal-title>
+            {{ headerTitle(alert.type) }}
+        </template>
+        {{ alert.message }}
+    </b-modal>
     <router-view></router-view>
   </div>
 </template>
@@ -9,6 +18,12 @@
   import { mapState, mapActions } from 'vuex'
   export default {
     name: 'app',
+    data(){
+      return{
+          headerBgVariant : function (type) { return type !== true ? "success" : "danger"},
+          headerTitle: function (type) { return type !== true ? "Successfully resisted" : "Error"}
+      }
+    },
     computed: {
       ...mapState({
         alert: state => state.alert
