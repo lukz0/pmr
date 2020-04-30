87adbe3ed149744e455167208b3d9b0e2a3e1692
Byamungu Kabiraba <byamuk16@student.uia.no>
2020-04-30 03:33:29 +0200

    added authorization

--- Robot_Sim_Api/app.py
+++ Robot_Sim_Api/app.py
@@ -2,7 +2,15 @@ from flask import Flask
 from flask_restplus import Api, Resource, fields
 from werkzeug.exceptions import *
 
-robot_base_url = 'api/v2.0.0'
+
+robot_base_url = '/api/v2.0.0'
+auth = {
+    'apikey': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'X-API-KEY'
+    }
+}
 
 app = Flask(__name__)
 api = Api(app,
@@ -10,7 +18,7 @@ api = Api(app,
           title='Robot API',
           description='Simulation API for MiR Robot',
           default_mediatype='application/json',
-          authorizations={},
+          authorizations=auth,
           contact_email='byamungukabiraba@gmail.com')
 
 ns = api.namespace('Robot API', description='TODO operations')
