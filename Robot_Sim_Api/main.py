be43d3c7ee5d0437ae6e4cf16ab1a9367a3ab402
Lukasz Filip Mrozik <lukafm18@student.uia.no>
2020-05-04 12:12:11 +0200

    made the mission queue run missions

--- Robot_Sim_Api/main.py
+++ Robot_Sim_Api/main.py
@@ -1,6 +1,7 @@
 from models.status import *
 from models.registers import *
 from models.mission import *
+from models.mission_queue import *
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Robot_Sim_Api/models/mission_queue.py
+++ Robot_Sim_Api/models/mission_queue.py
@@ -1,5 +1,6 @@
 from models.mission import *
-from heapq import heappop, heappush
+from threading import Semaphore, Thread
+from ThreadedHeap import ThreadedHeap
 
 getMissionQueues_model = api.model('GetMissionQueue', {
     'id': fields.Integer,
@@ -10,8 +11,8 @@ getMissionQueues_model = api.model('GetMissionQueue', {
 postMissionQueues_model = api.model('PostMissionQueue', {
     'fleet_schedule_guid': fields.String(max_length=36),
     'message': fields.String(max_length=200),
-    'mission_id': fields.String(min_length=1), # REQUIRED
-    'parameters': fields.List(fields.Wildcard),
+    'mission_id': fields.String(min_length=1),  # REQUIRED
+    'parameters': fields.Wildcard,
     'priority': fields.Float
 })
 
@@ -58,6 +59,7 @@ class MissionQueueItem:
         self.finished = None
         self.ordered = None
         self.allowed_methods = ['PUT', 'GET', 'DELETE']
+        self.aborted_callback = None
 
     def __str__(self):
         return str(self.__class__) + ': ' + str(self.__dict__)
@@ -68,6 +70,14 @@ class MissionQueueItem:
     def stop(self):
         if self.state in [state_pending[0], state_running[0]]:
             self.state = 'Aborted'
+            if self.aborted_callback is not None:
+                try:
+                    self.aborted_callback()
+                except TypeError:
+                    pass
+
+    def done(self):
+        self.state = state_done
 
     def get_actions(self):
         return '/v2.0.0/mission_queue/{}/actions'.format(self.id)
@@ -79,10 +89,21 @@ class MissionQueueItem:
 
     mission = property(get_mission)
 
+    def set_aborted_callback(self, callback):
+        self.aborted_callback = callback
+
+    def run(self):
+        self.state = state_running
+
+    def get_url(self):
+        return '/v2.0.0/mission_queue/{}'.format(self.id)
+
+    url = property(get_url)
+
 
 class MissionQueueDAOClass:
     def __init__(self):
-        self.pending_queue = []
+        self.pending_queue = ThreadedHeap()
         self.all_queue = []
         self.current_i = 0
 
@@ -98,10 +119,9 @@ class MissionQueueDAOClass:
             post_data.get('message', ''),
             post_data['mission_id'],
             post_data.get('parameters', []))
-        heappush(self.pending_queue, item)
+        self.pending_queue.heappush(item)
         self.all_queue.append(item)
-        # TODO: start the mission here if they do something
-        return item
+        return item.__dict__
 
     def stop_all(self):
         # TODO: if the missions do something, stop them
@@ -121,6 +141,23 @@ class MissionQueueDAOClass:
 missionQueueDAO = MissionQueueDAOClass()
 
 
+def mission_runner_func():
+    timer_semaphore = Semaphore(value=0)
+    while True:
+        item = missionQueueDAO.pending_queue.heappop()
+        item.set_aborted_callback(lambda: timer_semaphore.release())
+        item.run()
+        aborted = timer_semaphore.acquire(timeout=10)
+        item.set_aborted_callback(None)
+        if not aborted:
+            item.done()
+        timer_semaphore.acquire(blocking=False)
+
+
+mission_runner = Thread(target=mission_runner_func)
+mission_runner.start()
+
+
 @api.route('/json/v2.0.0/mission_queue')
 class MissionQueue(Resource):
     @auth_required
@@ -130,7 +167,17 @@ class MissionQueue(Resource):
 
     @auth_required
     @ns.expect(postMissionQueues_model)
+    @ns.marshal_with(getMissionSpecific_model)
     def post(self):
         if api.payload is not None:
             return missionQueueDAO.add(api.payload)
         api.abort(400, 'Request body needs to be application/json')
+
+
+@api.route('/json/v2.0.0/mission_queue/<int:i>')
+@ns.param('i', 'Mission id')
+class MissionQueueSpecific(Resource):
+    @auth_required
+    @ns.marshal_with(getMissionQueuesSpecific_model)
+    def get(self, i):
+        return missionQueueDAO.get(i)
