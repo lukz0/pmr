76db2726638ae39b532902625f3970d168c0effd
Lukasz Filip Mrozik <lukafm18@student.uia.no>
2020-05-17 17:46:17 +0200

    preparation to run robotsim on the server

--- Robot_Sim_Api/main.py
+++ Robot_Sim_Api/main.py
@@ -4,5 +4,15 @@ from models.registers import *
 from models.mission import *
 from models.mission_queue import *
 
+port = 0
+while True:
+    try:
+        port = int(input("enter port number"))
+        break
+    except ValueError:
+        print("port must be an integer")
+
 if __name__ == '__main__':
-    app.run(debug=True, port='5009')
+    # debug changed to False to avoid server restarts while being used by a script
+    # you can temporary change to true while developing to restart server automatically on code change
+    app.run(debug=False, port=port)
