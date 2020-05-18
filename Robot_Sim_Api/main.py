671ebdb88f60297302fb52dbabd7bc252804c354
Byamungu Kabiraba <byamuk16@student.uia.no>
2020-05-19 01:10:00 +0200

    robot name is displayd in docker

--- Robot_Sim_Api/main.py
+++ Robot_Sim_Api/main.py
@@ -4,5 +4,9 @@ from models.registers import *
 from models.mission import *
 from models.mission_queue import *
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port='5009')
+    # Test - TODO print the info in termical
+    host_name = 'MIR_2X23'
+    app.run(debug=True, port='5003')
