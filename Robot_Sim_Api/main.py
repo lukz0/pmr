7e6b7be67c33470cc65a6479386e6c4feda8c548
Lukasz Filip Mrozik <lukafm18@student.uia.no>
2020-05-20 15:06:49 +0200

    the robot simulator now listens to 0.0.0.0

--- Robot_Sim_Api/main.py
+++ Robot_Sim_Api/main.py
@@ -16,4 +16,4 @@ while True:
 if __name__ == '__main__':
     # debug changed to False to avoid server restarts while being used by a script
     # you can temporary change to true while developing to restart server automatically on code change
-    app.run(debug=False, port=port)
+    app.run(debug=False, port=port, host="0.0.0.0")
