829b86625819fe45bd8643e18ccd3067bb621f5e
Byamungu Kabiraba <byamuk16@student.uia.no>
2020-05-21 17:40:54 +0200

    test charts

---
+++ frontend/src/components/Dashboard/BarStatus.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <div class="small">
+            <bar-chart :chart-data="datacollection"></bar-chart>
+        </div>
+    </div>
+</template>
+
+<script>
+import BarChart from "./BarChart";
+
+export default {
+    name: "BarStatus",
+    components: {
+        BarChart
+    },
+    data() {
+        return {
+            datacollection: null
+        }
+    },
+    mounted() {
+        this.fillData()
+    },
+    methods: {
+        fillData() {
+            this.datacollection = {
+                labels: ["",""],
+                datasets: [
+                    {
+                        label: 'Robot3X-323',
+                        backgroundColor: '#007a98',
+                        data: [12,0]
+                    }, {
+                        label: 'MILRXX293',
+                        backgroundColor: '#dac7ff',
+                        data: [20,0]
+                    },
+                    {
+                        label: 'Rob4344-SS',
+                        backgroundColor: '#ffe744',
+                        data: [11,0]
+                    }
+                ]
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .small {
+        max-width: 700px;
+        height: 90px;
+        margin: 100px auto;
+    }
+</style>
--- frontend/src/components/Dashboard/Home.vue
+++ frontend/src/components/Dashboard/Home.vue
@@ -15,17 +15,26 @@
             </b-card>
             <b-card bg-variant="success" text-variant="white" class="text-center">
                 <template v-slot:header>
-                    <h2><b-icon-people/> Users</h2>
+                    <h2>
+                        <b-icon-people/>
+                        Users
+                    </h2>
                 </template>
                 <b-card-text><h1>32</h1></b-card-text>
             </b-card>
         </b-card-group>
+        <Status class="float-right"></Status>
+        <BarStatus class="float-right"></BarStatus>
     </b-jumbotron>
 </template>
 
 <script>
+    import Status from "./Status";
+    import BarStatus from "./BarStatus";
+
     export default {
-        name: "Home"
+        name: "Home",
+        components: {BarStatus, Status}
     }
 </script>
 
