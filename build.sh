e455a95648d37d8212ca8c2bc3f662db5f36cdd1
Lukasz Filip Mrozik <lukafm18@student.uia.no>
2020-05-23 17:26:57 +0200

    merge

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-dotnet publish backend/backend.csproj -c Release -f netcoreapp3.1 -o output --self-contained true -r linux-arm64
+$(cd frontend; yarn build)
+dotnet publish backend/backend.csproj -c Release -f netcoreapp3.1 -o output --self-contained true -r $1
 # use tar -zxvf output.tar.gz output to output as folder
 tar -zcvf output.tar.gz output/*
 rm -r output
