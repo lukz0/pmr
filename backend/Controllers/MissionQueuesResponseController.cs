110990f1a48b6177de0a3a9ce1ab805bc1f8c2d7
Byamungu Kabiraba <byamuk16@student.uia.no>
2020-05-24 21:19:58 +0200

    last change 12

---
+++ backend/Controllers/MissionQueuesResponseController.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+using backend.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace backend.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MissionQueuesResponseController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MissionQueuesResponseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MissionQueuesResponse
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MissionQueuesResponse>>> GetMissionQueuesResponse()
+        {
+            return await _context.MissionQueuesResponse.Include(r => r.Robot).ToListAsync();
+        }
+
+        // GET: api/MissionQueuesResponse/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MissionQueuesResponse>> GetMissionQueuesResponse(int id)
+        {
+            var missionQueuesResponse = await _context.MissionQueuesResponse.FindAsync(id);
+
+            if (missionQueuesResponse == null)
+            {
+                return NotFound();
+            }
+
+            return missionQueuesResponse;
+        }
+
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMissionQueuesResponse(int id, MissionQueuesResponse missionQueuesResponse)
+        {
+            if (id != missionQueuesResponse.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(missionQueuesResponse).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MissionQueuesResponseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+        
+
+        private bool MissionQueuesResponseExists(int id)
+        {
+            return _context.MissionQueuesResponse.Any(e => e.Id == id);
+        }
+    }
+}
