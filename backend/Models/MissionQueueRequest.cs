82bb8650f95ec4a55900c075ecc9dff464fee878
Byamungu Kabiraba <byamuk16@student.uia.no>
2020-05-24 15:29:02 +0200

    backend kan nå sende missions fra db til robot

---
+++ backend/Models/MissionQueueRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace backend.Models
+{
+    public class MissionQueueRequest
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+        
+        [JsonPropertyName("mission_id")]
+        public int MissionId { get; set; }
+        
+        [JsonIgnore]
+        public Mission Mission { get; set; }
+        
+        public string Name { get; set; }
+        public string Guid { get; set; }
+        public string Description { get; set; }
+    }
+}
