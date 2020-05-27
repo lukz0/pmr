074b7e0b616a76dcfd92ee364802a14ebb1afe6d
Lukasz Filip Mrozik <lukafm18@student.uia.no>
2020-05-27 15:12:21 +0200

    debugging

--- frontend/src/services/status.service.js
+++ frontend/src/services/status.service.js
@@ -10,3 +10,10 @@ function getByRobotId(id) {
     return axios.get(`/status/robotid=`+id)
         .then(r => requestService.handleResponse(r, null), e => requestService.handleResponse(e.response, e))
 }
+
+function getAllStates() {
+    return axios.get(`/status`)
+        .then(r => requestService.handleResponse(r, null), e => requestService.handleResponse(e.response, e));
+}
+
+window.getAllStates = getAllStates;
